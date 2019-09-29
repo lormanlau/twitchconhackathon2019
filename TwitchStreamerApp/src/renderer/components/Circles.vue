39ca7478377e78a0efada4900bd9cd3c55c938ef
William Tran <billvctt@gmail.com>
2019-09-29 07:52:42 -0700

    Added Twitch Streamer App

---
+++ TwitchStreamerApp/src/renderer/components/Circles.vue
@@ -0,0 +1,62 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  import * as d3 from 'd3';
+
+  export default {
+    name: 'Circles',
+    created() {
+    },
+    mounted() {
+      this.svg = d3.select(document.getElementById('svg'));
+
+      this.loopID = setInterval(() => {
+        this.make_it_rain(2);
+      }, 250);
+    },
+    methods: {
+      make_it_rain(numDrops) {
+        // console.log('Making it rain');
+        for (let i = 0; i < numDrops; i += 1) {
+          let size = 50 * Math.random();
+          size += 50;
+          let duration = 50 * Math.random();
+          duration += 750;
+          const xPos = window.innerWidth * Math.random();
+          const yPos = window.innerHeight * Math.random();
+          this.raindrop(size, duration, xPos, yPos);
+        }
+      },
+
+      raindrop(size, duration, xPos, yPos) {
+        const drop = this.svg.append('circle')
+          .attr('cx', xPos)
+          .attr('cy', yPos)
+          .attr('r', 0)
+          .attr('stroke', 'black')
+          .attr('stroke-width', 5)
+          // .attr('fill', '#5FC3E4')
+          .style('fill', () => `hsl(${Math.random() * 360},100%,50%)`)
+          // .attr('fill', 'none')
+          .attr('opacity', 1);
+
+        drop.transition()
+          .duration(duration)
+          .attr('r', size)
+          // .attr('stroke-width', 0)
+          // .attr('opacity', 0.5)
+          .ease(Math.sqrt)
+          .remove();
+      },
+    },
+    destroyed() {
+      clearInterval(this.loopID);
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ TwitchStreamerApp/src/renderer/components/Main.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+      id="wrapper"
+      :style="bgStyle">
+    <challenges
+        v-if="landed"
+        @bg="updateBg"/>
+    <landing-page
+        v-else
+        @finish="transitionToWindowless"/>
+  </div>
+</template>
+
+<script>
+  /* eslint-disable no-console */
+
+  import { remote } from 'electron';
+  import LandingPage from './LandingPage';
+  import Challenges from './Challenges';
+
+  export default {
+    name: 'Main',
+    components: { Challenges, LandingPage },
+    data() {
+      return {
+        landed: false,
+
+        greyChallenges: false,
+        challengeRatePerTen: 1,
+        bgStyle: {
+          'background-color': 'rgba(0, 0, 0, 0) !important',
+        },
+      };
+    },
+    created() {
+      if (this.landed) this.transitionToWindowless();
+    },
+    methods: {
+      transitionToWindowless() {
+        const mainWindow = remote.getCurrentWindow();
+        mainWindow.setVisibleOnAllWorkspaces(true);
+        mainWindow.setAlwaysOnTop(true, 'floating', 1);
+        mainWindow.setFullScreenable(false);
+        // mainWindow.setIgnoreMouseEvents(true, { forward: true });
+        this.$set(this, 'landed', true);
+      },
+
+      updateBg(newColor) {
+        console.log(this.bgStyle['background-color']);
+        this.$set(this.bgStyle, 'background-color', `${newColor} !important`);
+      },
+    },
+  };
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  @font-face {
+    font-family: 'dpcomic';
+    src: url('../../../static/dpcomic.regular.ttf');
+  }
+
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    overflow: hidden;
+  }
+
+  #wrapper {
+    /*background:*/
+    /*radial-gradient(*/
+    /*ellipse at top left,*/
+    /*rgba(255, 255, 255, 1) 40%,*/
+    /*rgba(229, 229, 229, .9) 100%*/
+    /*);*/
+    height: 100vh;
+    width: 100vw;
+  }
+
+  m-auto {
+    margin: auto;
+  }
+</style>
